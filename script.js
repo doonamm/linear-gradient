b1a26b5c64458e1a8ebe23309f82553cedf44430
Nam Ant <83125103+doonamm@users.noreply.github.com>
2021-07-23 21:29:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -620,7 +620,7 @@ function showLoading(e){
 
     const {clientHeight, scrollTop, scrollHeight} = document.documentElement;
     
-    if(clientHeight + scrollTop + 10 >= scrollHeight){
+    if(clientHeight + scrollTop + 30 >= scrollHeight){
         if(!showing){
             showing = true;
             loading.classList.add('show');
